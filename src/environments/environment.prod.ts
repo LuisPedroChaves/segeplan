696bcc14c2fb2362901a7c1b29ed74446cd7abef
LuisPedroChaves <luispedrochaves@hotmail.com>
2022-07-25 15:51:38 -0600

    Primer commit con la configuracion inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  name: 'prod',
+  production: true,
+
+  /* API Services Config */
+  root: 'http://localhost:3000/',
+
+  /* Version Config */
+  version: '1.0',
+  baseurl: '/',
 };
