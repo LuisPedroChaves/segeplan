a67dfcaf2e38cf1b6ab42216ea7340f8c66c895c
Geovany Ocaña <egoo.geovany@gmail.com>
2022-07-31 02:54:28 -0600

    Models General Information

--- src/app/core/models/GeneralInformation.ts
+++ src/app/core/models/GeneralInformation.ts
@@ -1,24 +1,35 @@
-import { SectionBI } from "./section";
+import { DataProponent } from "./DataProponent";
+import { PreliminaryDefinition } from "./PreliminaryDefinition";
+import { ProblemDefinition } from "./ProblemDefinition";
+import { Qualification } from "./Qyualification";
+import { Stage } from "./Stage";
 
 export interface GeneralInformationSend {
     codigo?: string;
-    sectionBI: string;
     productId: string; //Setear valor quemado
     productName: string; //Setear valor quemado
     date: Date;
     registerCode: string;
+    correlation: number;
     planningInstrument: boolean;
     description: string;
 }
 
 export interface GeneralInformation {
     codigo?: string;
-    sectionBI: string;
     productId: string; //Setear valor quemado
     productName: string; //Setear valor quemado
     date: Date;
+    correlation: number;
     registerCode: string;
     planningInstrument: boolean;
     description: string;
-	_sectionBI: SectionBI;
+    dateOut: Date;
+    punctuation: number;
+    state: boolean;
+    stage: Stage;
+    DataProponent: DataProponent;
+    ProblemDefinition: ProblemDefinition;
+    PreliminaryDefinition: PreliminaryDefinition;
+    Qualification: Qualification;
 }
---
+++ src/app/core/models/Qyualification.ts
@@ -0,0 +1,8 @@
+export interface Qualification {
+    codigo?: string;
+    descriptionProblem?: number;
+    generalObjective?: number;
+    analysisDelimitation?: number;
+    terrainIdentification?: number;
+    descriptionAnalysis?: number;
+}
