9c709d268d317d7a96dc90db3ef273b5aaf8cde8
Geovany <egoo.geovany@gmail.com>
2022-11-25 13:13:53 -0600

    Pequeños Cambios

--- src/app/modules/check-project/pages/project-list/project-list.component.ts
+++ src/app/modules/check-project/pages/project-list/project-list.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 import { MatTableDataSource } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 
 import { IProject } from 'src/app/core/models/seguimiento/project';
 import { ChekProjectService } from '../../../../core/services/httpServices/chek-project.service';
+import { OPEN_FORM_DRAWER } from '../../../../store/actions';
+import { AppState } from '../../../../store/app.reducer';
 
 @Component({
   selector: 'app-project-list',
@@ -12,6 +17,10 @@ import { ChekProjectService } from '../../../../core/services/httpServices/chek-
 })
 export class ProjectListComponent implements OnInit {
 
+  storeSubscription = new Subscription;
+  @ViewChild('formDrawer') formDrawer!: MatDrawer;
+
+
   projects: IProject[];
 
   displayedColumns = ['process', 'sector', 'munic', 'nameProject', 'advance', 'actions'];
@@ -19,21 +28,32 @@ export class ProjectListComponent implements OnInit {
   dataSource = new MatTableDataSource<IProject>([]);
 
   constructor(
+    public store: Store<AppState>,
     private chekProjectService: ChekProjectService,
   ) { }
 
   ngOnInit(): void {
+    this.storeSubscription = this.store.select('drawer')
+      .subscribe(state => {
+        if (this.formDrawer) {
+          this.formDrawer.opened = state.formDrawer
+        }
+      });
     this.getProjects();
-
   }
 
   getProjects(): void {
     this.chekProjectService.getAllProjects()
-    .subscribe(data => {
-      this.projects = data;
-      this.dataSource = new MatTableDataSource<IProject>(this.projects);
-      console.log(this.dataSource)
-    })
+      .subscribe(data => {
+        this.projects = data;
+        this.dataSource = new MatTableDataSource<IProject>(this.projects);
+        console.log(this.dataSource)
+      })
+  }
+
+  openFormDrawer(formTitle: string, formComponent: string): void {
+    console.log('Entro a la funcion')
+    this.store.dispatch(OPEN_FORM_DRAWER({ formTitle, formComponent }))
   }
 
 }
